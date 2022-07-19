e8a0c9b33b256b71b5faa85c4453bb25ad9dcb34
Leandro Garcia <leandrogabrielgarcia@gmail.com>
2022-07-19 17:23:13 -0300

    adding maui, folder structure, file upload in frontend and backend.

---
+++ Shared/Folder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Shared
+{
+    public class Folder
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public int? ParentFolderId { get; set; }
+        public Folder? ParentFolder { get; set; }
+        public virtual List<Folder> SubFolders { get; set; } = new List<Folder>();
+        public bool IsExpanded { get; set; }
+    }
+}
