af2ff3255d2fc87a175380b2bd2f8cbfb5c2761b
Leandro Garcia <leandrogabrielgarcia@gmail.com>
2022-07-20 17:20:41 -0300

    Renegando con choca para mostrar bien el HTml. Ya muestra el folder structure en una estructura logica.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 using BlazorApp1.Client;
 using BlazorApp1.Client.Repository;
+using KristofferStrube.Blazor.FileSystemAccess;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton(sp => new SingletonService());
 builder.Services.AddSingleton<IRepository, Repository>();
+builder.Services.AddFileSystemAccessService();
 
 await builder.Build().RunAsync();
--- Shared/Folder.cs
+++ Shared/Folder.cs
@@ -8,11 +8,23 @@ namespace BlazorApp1.Shared
 {
     public class Folder
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
-        public int? ParentFolderId { get; set; }
         public Folder? ParentFolder { get; set; }
         public virtual List<Folder> SubFolders { get; set; } = new List<Folder>();
+        public virtual List<string> Files { get; set; } = new List<string>();
         public bool IsExpanded { get; set; }
+    }
+
+    public class FileM
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid FolderId { get; set; }
+
+        public string Path { get; set; }
     }
 }
